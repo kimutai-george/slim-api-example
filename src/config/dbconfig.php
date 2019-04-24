6d5cf0254716b7b63fc71e14a8d4ee0df3c435a1
George Kimutai <georgekimuu@gmail.com>
2019-04-24 22:57:45 +0300

    slim1

--- src/config/dbconfig.php
+++ src/config/dbconfig.php
@@ -2,11 +2,24 @@
 
 class dbconfig
 {
+
+	//database
+	//use the following sql command to create your table
+		/*CREATE TABLE `customers` (
+	  `id` int(12) NOT NULL AUTO_INCREMENT,
+	  `first_name` varchar(20) DEFAULT NULL,
+	  `last_name` varchar(20) DEFAULT NULL,
+	  `phone_number` varchar(30) DEFAULT NULL,
+	  `email` varchar(30) DEFAULT NULL,
+	  PRIMARY KEY (`id`)
+	) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;*/
+
+
 	//db properties
 
 	private $db_host = 'localhost';
 	private $db_user =  'root';
-	private $db_pass = '12??_??12kim';
+	private $db_pass = '';
 	private $db_name = 'testtest';
 
 	public function connect()
